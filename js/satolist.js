f7042d3443922f84a9aaab1da8fd0b9953da3d68
Mo <MoMentalochka@yandex.ru>
2022-08-17 15:19:52 +0300

    old changes

--- js/satolist.js
+++ js/satolist.js
@@ -4698,10 +4698,6 @@ Platform = function (app, listofnodes) {
                         })
 
                         el.find('.block').on('click', function () {
-                            if (self.app.platform.sdk.node.transactions.hasUnspentMultyBlocking()){
-                                sitemessage(self.app.localization.e('blockinginprogress'))
-                                return
-                            }
                             self.app.mobile.vibration.small()
 
                              self.api.actions.blocking(address, function (tx, error) {
@@ -4710,7 +4706,7 @@ Platform = function (app, listofnodes) {
                                  }
                              })
                             dialog({
-                                html: self.app.localization.e('blockingdisclaimer'),
+                                html: "Do you want to also block connected accounts? ONLY do this for suspected bots.",
                                 btn1text: "Yes",
                                 btn2text: "No",
                                 class: 'zindex',
@@ -18351,13 +18347,6 @@ Platform = function (app, listofnodes) {
                             c(-amount);
                         })
                     }
-                },
-
-                hasUnspentMultyBlocking: function() {
-                    var s = Object.values(self.sdk.node.transactions.temp.blocking)
-                    return s.some(blocking => {
-                        return typeof blocking.vsaddress === 'object'
-                    })
                 },
 
                 get: {
